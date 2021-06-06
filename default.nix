47024586a3c6e821382b6e910e0d67d34240d329
rebecca skinner <rebecca@rebeccaskinner.net>
2021-06-06 18:06:30 -0500

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
 { doCheck ? true
-# , pkgs ? import <nixpkgs> {}
 }:
 
 let
-  pkgs = import <nixpkgs> {
+  pkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable-2020-06-06";
+    url = "https://github.com/nixos/nixpkgs/archive/7e9e1b6351bb4acc71f88d3fc4defa3fa899e121.tar.gz";
+    sha256 = "1ga7zkkzksgpvymkblj31m55zdrn1ak2iqnisk177x5mgd9vvcqp";
+  }){
     config = { allowBroken = true; };
-    overlays = [(import ./nix/hakyll)];
+     overlays = [(import ./nix/hakyll)];
   };
+
   builder = import ./builder { inherit pkgs; };
   built = pkgs.stdenv.mkDerivation {
     name = "rebeccaskinner.net";
@@ -18,14 +22,18 @@ let
       "*.hs"
       ".github"
     ] ./.;
-
     buildInputs = [ builder pkgs.linkchecker ];
+    LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+    LC_ALL = "C.UTF-8";
     buildPhase = ''
       ${builder}/bin/rebeccaskinner-net-site build
-'';
+    '';
+    checkPhase = ''
+      linkchecker _site
+    '';
     installPhase = ''
       cp -r _site $out
-'';
+    '';
   };
 in
 if pkgs.lib.inNixShell then builder
